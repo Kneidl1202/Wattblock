50dfa3c8a25f6178585b7e57f566e5556abd1043
Kneidl1202 <andreas.auer03@gmail.com>
2022-08-15 08:44:52 +0200

    initial commit

---
+++ Wattblock/Wattblock/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Wattblock
+//
+//  Created by Andreas Auer on 15/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
