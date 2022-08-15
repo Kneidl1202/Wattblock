0279daf3d80b78c02cddc551f35269d1e3371bca
Kneidl1202 <andreas.auer03@gmail.com>
2022-08-15 08:58:50 +0200

    initial commit

---
+++ Wattblock/Wattblock/WattblockApp.swift
@@ -0,0 +1,17 @@
+//
+//  WattblockApp.swift
+//  Wattblock
+//
+//  Created by Andreas Auer on 15/08/22.
+//
+
+import SwiftUI
+
+@main
+struct WattblockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
